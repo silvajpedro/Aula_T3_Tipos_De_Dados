88177e85f36566f25712f1f00bcdaf10b17219ce
silvajpedro <belojpedro.31@gmail.com>
2025-01-31 21:15:25 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,53 @@
+# Variáveis --> Funcionam como pequenas caixas que armazenam informações essenciais para nosso código ou aplicação.
+
+lanche_do_sextou = "pizza de cabralesa com frango catupiry"  # Exemplo de variável armazenando uma string
+
+valor_pi = 3.14  # Exemplo de variável armazenando um número decimal (float)
+
+# Convenção camelCase para nomeação de variáveis
+sobremesaDoSextou = "Mousse de Maracujá"  
+
+# 📌 Tipos de Dados em Python 📌  
+
+# 🔹 Inteiros (int)  
+# Representam números inteiros, positivos ou negativos, sem casas decimais.  
+
+divida_fernanda = -1000  # Número inteiro negativo
+
+salario_alunos = 10**60  # Número inteiro extremamente grande, ilustrando a capacidade do Python
+
+# 🔹 Ponto flutuante (float)  
+# Representam números decimais, usando ponto como separador.  
+
+pastel_de_belem = 5.99  # Valor decimal positivo  
+
+pi = -3.14  # Valor decimal negativo  
+
+# 🔹 Strings (str)  
+# São cadeias de caracteres, sempre envolvidas por aspas simples ('') ou duplas ("").  
+
+fernanda = "Com apenas 18 anos já dominou as artes místicas do front-end"  # Exemplo de string  
+
+# 🔹 Booleanos (bool)  
+# Representam valores lógicos: Verdadeiro (True) ou Falso (False).  
+
+tem_saldo = False  # Indica que não há saldo disponível  
+
+fernanda_deve = True  # Indica que a Fernanda está em débito  
+
+# 🔹 Nulos (None)  
+# Representam a ausência de valor ou uma variável ainda não inicializada.  
+
+vazio = None  # Variável iniciada sem valor  
+
+vazio = "pizza"  # A variável agora recebe um valor  
+
+# 🔹 Input  
+# Permite a interação com o usuário via terminal, capturando valores digitados.  
+
+primeiro_valor = input("Digite o primeiro valor: ")  # Captura um valor do usuário
+
+segundo_valor = input("Digite o segundo valor: ")  # Captura um segundo valor
+
+# ⚠️ Atenção: O input retorna um valor do tipo string, então a soma abaixo será uma concatenação de texto, não uma soma matemática.
+print(primeiro_valor + segundo_valor)  # Exemplo de saída
